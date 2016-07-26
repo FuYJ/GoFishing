35ce1f32fe09951298a7915666f156b972abced3
102820021 <102820021wsx987@gmail.com>
2016-07-26 17:39:27 +0800

    Update ScenesData.cs

--- GoFishing/Assets/Script/ScenesData.cs
+++ GoFishing/Assets/Script/ScenesData.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class ScenesData : MonoBehaviour {
@@ -9,6 +9,13 @@ public class ScenesData : MonoBehaviour {
 
     private XBikeEventReceiver.ConnectionStatus connectionStatus = XBikeEventReceiver.ConnectionStatus.Disconnected;
     private XBikeEventReceiver.SportStatus sportStatus = XBikeEventReceiver.SportStatus.Stop;
+    public bool test = false;
+
+    public ScenesData()
+    {
+        XBikeEventReceiver.connectStatusChangeEvent += OnXBikeConnectionStatusChange;
+        XBikeEventReceiver.sportStatusChangeEvent += OnXBikeSportStatusChange;
+    }
 
 #if UNITY_EDITOR
     public void SendMessageForEachListener(string method, string arg)
@@ -56,7 +63,7 @@ public class ScenesData : MonoBehaviour {
         }
     }
 
-    private void StopSport()
+    public void StopSport()
     {
 #if UNITY_EDITOR
         SendMessageForEachListener("OnXBikeSportStatusChange", "0");
@@ -65,7 +72,7 @@ public class ScenesData : MonoBehaviour {
 #endif
     }
 
-    private void StartSport()
+    public void StartSport()
     {
 #if UNITY_EDITOR
         SendMessageForEachListener("OnXBikeSportStatusChange", "1");
@@ -74,12 +81,79 @@ public class ScenesData : MonoBehaviour {
 #endif
     }
 
-    private void PauseSport()
+    public void PauseSport()
     {
 #if UNITY_EDITOR
         SendMessageForEachListener("OnXBikeSportStatusChange", "2");
 #elif UNITY_ANDROID
 		XBikeEventReceiver.PauseSport();
 #endif
+    }
+
+    //要測試是否可以成為事件
+    /// <summary>
+    /// called when there is a change in status
+    /// </summary>
+    /// <param name='status'>
+    /// "0" = stop / "1" = start / "2" = pause
+    /// </param>
+    public void OnXBikeSportStatusChange(string status)
+    {
+        sportStatus = (XBikeEventReceiver.SportStatus)int.Parse(status);
+        switch (sportStatus)
+        {
+            case XBikeEventReceiver.SportStatus.Stop:
+                {
+                    sportStatus = XBikeEventReceiver.SportStatus.Stop;
+                    break;
+                }
+            case XBikeEventReceiver.SportStatus.Pause:
+                {
+                    sportStatus = XBikeEventReceiver.SportStatus.Pause;
+                    break;
+                }
+            case XBikeEventReceiver.SportStatus.Start:
+                {
+                    sportStatus = XBikeEventReceiver.SportStatus.Start;
+                    break;
+                }
+        }
+    }
+
+    /// <summary>
+	/// called when the connectivity changes
+	/// </summary>
+	/// <param name='status'>
+	/// "0" = disconnect / "1" = connecting / "2" = connected
+	/// </param>
+	private void OnXBikeConnectionStatusChange(string status)
+    {
+        connectionStatus = (XBikeEventReceiver.ConnectionStatus)int.Parse(status);
+        switch (connectionStatus)
+        {
+            case XBikeEventReceiver.ConnectionStatus.Connecting:
+                {
+#if UNITY_EDITOR
+                    //				StartCoroutine(WaitTime(3.0f));
+#endif
+                    break;
+                }
+            case XBikeEventReceiver.ConnectionStatus.Connected:
+                {
+                    break;
+                }
+        }
+    }
+
+    public bool Test
+    {
+        get
+        {
+            return test;
+        }
+        set
+        {
+            test = true;
+        }
     }
 }
