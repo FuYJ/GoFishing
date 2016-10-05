d9995416a37738da84b928c8e1a360d3d337f278
FuYJ <zz28234830@gmail.com>
2016-10-06 02:05:09 +0800

    Add: stage description

--- GoFishing/Assets/Script/SelectStage/StageButton.cs
+++ GoFishing/Assets/Script/SelectStage/StageButton.cs
@@ -1,8 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class StageButton : MonoBehaviour {
 
+	public GameObject m_stageDescription;
 	private SceneLoader.Scenes _stageIndex;
 
 	public SceneLoader.Scenes StageIndex{
@@ -16,8 +18,19 @@ public class StageButton : MonoBehaviour {
 
 	public void OnStageButtonClick(){
 		SoundManager.Instance.PlayClickSound ();
+		StartCoroutine(PopUpStageDescription(GameManager.Instance.m_sceneNames.scenes[(int)_stageIndex].stageDescription));
+		/*SoundManager.Instance.PlayClickSound ();
 		SelectStageScript _selectStageScript;
 		_selectStageScript = GameObject.Find ("SelectStageScript").GetComponent<SelectStageScript>();
-		_selectStageScript.LoadStage (_stageIndex);
+		_selectStageScript.LoadStage (_stageIndex);*/
+	}
+
+	private IEnumerator PopUpStageDescription(string msg){
+		GameObject newDescription = Instantiate (m_stageDescription);
+		Text stageDescription = newDescription.GetComponentInChildren<Text> ();
+		StageDescriptionScript stageDescriptionScript = newDescription.GetComponent<StageDescriptionScript> ();
+		stageDescription.text = msg;
+		stageDescriptionScript.StageIndex = _stageIndex;
+		yield break;
 	}
 }
